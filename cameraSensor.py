399012ade886f1214ed8f6c439e36daf30811a34
Louisse <louisseeustaquio@gmail.com>
2018-02-24 16:13:22 +0800

    tabs to space

--- cameraSensor.py
+++ cameraSensor.py
@@ -8,22 +8,22 @@ from time import sleep
 url='http://localhost/camera/receiveData.php'
 cap = cv2.VideoCapture(0)
 while(True):
-	if cap.isOpened():
-	# Capture frame-by-frame
-		ret, frame = cap.read()
-		if ret:
-			frame = cv2.resize(frame, (160, 120))
-			image = BytesIO(cv2.imencode('.png', frame)[1])
-			image.name = datetime.now().strftime('%Y-%m-%d %H-%M-%S')+'camera.png'
-			files={'camera': image}
-			r = requests.post(url, files=files)
-			print(r.text)
+  if cap.isOpened():
+  # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret:
+      frame = cv2.resize(frame, (160, 120))
+      image = BytesIO(cv2.imencode('.png', frame)[1])
+      image.name = datetime.now().strftime('%Y-%m-%d %H-%M-%S')+'camera.png'
+      files={'camera': image}
+      r = requests.post(url, files=files)
+      print(r.text)
 
-			# Display the resulting frame
-			cv2.imshow('frame',frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-	sleep(2.0)
+      # Display the resulting frame
+      cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+  sleep(2.0)
 
 # When everything done, release the capture
 cap.release()
